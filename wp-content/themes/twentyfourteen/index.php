5cd95470b6cc7a3576cf83e91134459eb21d16a0
bunrong <bunrongleang@gmail.com>
2015-04-19 20:11:54 +0700

    adding template

--- wp-content/themes/twentyfourteen/index.php
+++ wp-content/themes/twentyfourteen/index.php
@@ -16,40 +16,116 @@
 
 get_header(); ?>
 
-<div id="main-content" class="main-content">
-
-<?php
-	if ( is_front_page() && twentyfourteen_has_featured_posts() ) {
-		// Include the featured content template.
-		get_template_part( 'featured-content' );
-	}
-?>
-
-	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
-
-		<?php
-			if ( have_posts() ) :
-				// Start the Loop.
-				while ( have_posts() ) : the_post();
-
-					/*
-					 * Include the post format-specific template for the content. If you want to
-					 * use this in a child theme, then include a file called called content-___.php
-					 * (where ___ is the post format) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-
-				endwhile;
-				// Previous/next post navigation.
-				twentyfourteen_paging_nav();
-
-			else :
-				// If no content, include the "No posts found" template.
-				get_template_part( 'content', 'none' );
-
-			endif;
-		?>
+<!--This part for put in index.php-->
+<div class="container"><!--Contianer Content-->
+<!--Promotion Content-->
+  <div class="row" id="content"><!--Content-->
+     <div class="col-xs-12 col-sm-12 col-lg-12"><!--center-->
+              <div class="col-xs-6 col-sm-3 col-lg-3" id="promotion">
+                <center>
+                  <img src="<?php echo get_template_directory_uri();?>/images/promotion.png" class="imagelogo"/><br/>
+                  <b class="logotitle">Promotion1</b>
+                </center>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p> 
+              </div>
+              <div class="col-xs-6 col-sm-3 col-lg-3" id="promotion">
+                 <center>
+                  <img src="<?php echo get_template_directory_uri();?>/images/promotion.png" class="imagelogo"/><br/>
+                  <b class="logotitle">Promotion2</b>
+                </center>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+              </div>
+              <div class="col-xs-6 col-sm-3 col-lg-3" id="promotion">
+                 <center>
+                  <img src="<?php echo get_template_directory_uri();?>/images/promotion.png" class="imagelogo"/><br/>
+                  <b class="logotitle">Promotion3</b>
+                </center>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+              </div>
+              <div class="col-xs-6 col-sm-3 col-lg-3" id="promotion">
+                 <center>
+                  <img src="<?php echo get_template_directory_uri();?>/images/promotion.png" class="imagelogo"/><br/>
+                  <b class="logotitle">Promotion4</b>
+                </center>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+              </div>
+     </div><!--End col-->
+</div><!--End Container content-->
+<!--End Promotion Content-->
+<!--Content for Video and link-->
+<div class="row" id="content"><!--Content-->
+<div class="col-xs-12 col-sm-12 col-lg-12"><!--center-->
+         <div class="panel panel-default" id="video">
+          <div class="panel-body">
+            <b>Video and Imgae Gallary</b>
+            <div class="row">
+              <div class="col-xs-12 col-sm-12 col-lg-8">
+                <div class="col-xs-5 col-sm-5 col-lg-5" id="promotion">
+                  <img src="<?php echo get_template_directory_uri();?>/images/video.jpg" width="100%" />
+                </div>
+                <div class="col-xs-5 col-sm-5 col-lg-5" id="promotion">
+                  <img src="<?php echo get_template_directory_uri();?>/images/video.jpg" width="100%" />
+                </div>
+                <div class="col-xs-5 col-sm-5 col-lg-5" id="promotion">
+                  <img src="<?php echo get_template_directory_uri();?>/images/video.jpg" width="100%" />
+                </div>
+                <div class="col-xs-5 col-sm-5 col-lg-5" id="promotion">
+                  <img src="<?php echo get_template_directory_uri();?>/images/video.jpg" width="100%" />
+                </div>
+              </div>
+              <div class="col-xs-12 col-sm-12 col-lg-4">
+                <b>Quick Link</b>
+                <p>
+                  <ul>
+                    <li>Link1</li>
+                    <li>Link2</li>
+                    <li>Link3</li>
+                    <li>Link4</li>
+                  </ul>
+                </p>
+              </div><!--End col-->
+            </div><!--End row-->
+          </div><!--End panel body-->
+        </div><!--End Panel-->
+     </div><!--End col-->
+</div><!--End Container content-->
+<!--End content for video and link-->
+<!----Content for Sponcer-->
+<div class="row" id="content"><!--Content-->
+<div class="col-xs-12 col-sm-12 col-lg-12"><!--center-->
+         <div class="panel panel-default" id="sponcer">
+          <div class="panel-body">
+            <div class="row">
+              <div class="col-xs-12 col-sm-12 col-lg-12">
+                <b>Sponcer</b>
+                <center>
+                  <img src="<?php echo get_template_directory_uri();?>/images/khmer-logo.png" width="10%" class="sponcer-logo"/>
+                  <img src="<?php echo get_template_directory_uri();?>/images/khmer-logo.png" width="10%" class="sponcer-logo"/>
+                  <img src="<?php echo get_template_directory_uri();?>/images/khmer-logo.png" width="10%" class="sponcer-logo"/>
+                  <img src="<?php echo get_template_directory_uri();?>/images/khmer-logo.png" width="10%" class="sponcer-logo"/>
+                </center>
+              </div>
+            </div><!--End row-->
+          </div><!--End panel body-->
+        </div><!--End Panel-->
+     </div><!--End col-->
+</div><!--End Container content-->
+<!--End Content for sponcer-->
+</div><!--End Contain-->
+</div><!--End Row-->
+<!--End Part for index.php-->
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
